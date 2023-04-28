f136a9a2002bb56d8bd5c816bee432bc64a62992
anilcosarss <anilcosarsg12@gmail.com>
2023-04-29 01:15:52 +0300

    second commitsdssq

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Meal from './Components/Meal';
-import { Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import RecipeInfo from './Components/RecipeInfo';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Routes>
-        <Route path='/' element={ <Meal/>} />
-        <Route path='/:MealId' element={ <RecipeInfo/>} />
+        <Route path="/" element={<Meal />} />
+        <Route exact path="/:MealId" element={<RecipeInfo />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
