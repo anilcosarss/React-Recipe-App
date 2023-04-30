21da7d16691e68d4bf7934b8a6c09dc24bcbf806
anilcosarss <anilcosarsg12@gmail.com>
2023-05-01 00:33:08 +0300

    son commit2

--- src/Components/RecipeIndex.jsx
+++ src/Components/RecipeIndex.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function RecipeIndex({alphaIndex}) {
     const alpha = ['A', "B", "C", "D", "E", "F", "J", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var num=0;
+
   return (
     <div className='flex flex-wrap'>
         {
         alpha.map((letter =>(
             
-            <div key={num++} onClick={()=>alphaIndex(letter)} className="cursor-pointer flex justify-center rounded-lg items-center h-8 w-8  bg-black text-white mx-3 my-1">
+            <div key={letter} onClick={()=>alphaIndex(letter)} className="cursor-pointer flex justify-center rounded-lg items-center h-8 w-8  bg-black text-white mx-3 my-1">
                 <h3 >{letter}</h3>
             </div>
         )))
